fa381da4f2e4a856b93a21d78bca88da413da117
cristiandevar <cristianprogramadorunsa@gmail.com>
2024-09-02 21:53:04 -0300

    Pudimos integrar TailAdmin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/js/app.tsx',
+                'resources/css/app.css'
+            ]
         }),
+        react(),
     ],
 });
